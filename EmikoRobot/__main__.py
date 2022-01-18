61f86cdabc1a4a7d76674d4ff7f7ce8f3c890096
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-01-18 16:06:30 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -82,7 +82,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/c46b5dfde1e95777965ac.jpg)
+✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/96c01608cad3a46a280a2.jpg)
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
