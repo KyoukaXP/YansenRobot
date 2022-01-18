5cf0dbee9413db2d1a1c84a862d609e2fca92a06
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-01-18 17:01:08 +0700

    Update apakah.py

--- EmikoRobot/modules/apakah.py
+++ EmikoRobot/modules/apakah.py
@@ -26,23 +26,22 @@ async def apakah(event):
 
 
 
-YANSEN_STRING = ["ganteng", 
-                 "Manies", 
-                 "Baik", 
-                 "Jarang Sange", 
-                 "Kenapa Manggil Manggil?", 
-                 "Mungkin Tidak",
-                 "Gausa Sokab",
-                 "YNTKTS",
-                 ]
-
-
-
-
-@register(pattern="^/yansei ?(.*)")
+YANSEN_STRING = @register(pattern="^yansen ?(.*)")
 async def apakah(event):
     quew = event.pattern_match.group(1)
     if not quew:
-        await event.reply('Berikan saya pertanyaan 😐')
+        await event.reply('APA KONTOL')
         return
     await event.reply(random.choice(YANSEN_STRING))
+
+
+
+
+@register(pattern="^yansen ganteng ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('owner ganteng @yansesad')
+        return
+    await event.reply(random.choice(YANSEN_STRING))
+
